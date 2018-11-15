d5f77d8832f510ed3eefdbc3a65a57e827043f27
wippyzulkarnain <wippy.zulkarnain@gmail.com>
2018-11-15 10:34:17 +0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      image: null
+    };
+  }
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleImageChange = e => {
+    this.setState({ image: e.target.files[0] });
+  };
+  handleSubmit = e => {
+    const data = new FormData();
+    data.append(
+      "user.data",
+      JSON.stringify({ name: this.state.name, email: this.state.email })
+    );
+    data.append("user.image",this.state.image)
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <input
+          type="text"
+          name="name"
+          onChange={this.handleChange}
+          value={this.state.name}
+        />
+        <input
+          type="email"
+          name="email"
+          onChange={this.handleChange}
+          value={this.state.name}
+        />
+        <input type="file" name="image" onChange={this.handleImageChange} />
+        <button onClick={this.handleSubmit}>Submit</button>
       </div>
     );
   }
